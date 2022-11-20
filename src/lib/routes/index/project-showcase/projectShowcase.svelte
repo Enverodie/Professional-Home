ba20ca9490bce2259f0849ca520039980ecc4d4a
enverodie <enverodie@gmail.com>
2022-11-20 09:39:08 -0500

    Completed 3-showcase component for desktop

--- src/lib/routes/index/project-showcase/projectShowcase.svelte
+++ src/lib/routes/index/project-showcase/projectShowcase.svelte
@@ -3,44 +3,85 @@
 </script>
 
 <section class="showcase">
-    <div class="showcaseItem showcase1"><div class="innerShowcaseBorder"><div class="innerShowcase">
-        Showcase 1
+    <div class="showcaseColumn showcase1">
+        <div class="innerShowcaseBorder">
+            <div class="innerShowcase">
+                <ShowcaseItem class="innerShowcase1" href="#" item={1} backgroundImage='/static/images/showcase1_thumbnail.png' />
+            </div>
+        </div>
     </div>
-    </div></div>
-    <div class="showcaseItem showcase2"><div class="innerShowcaseBorder"><div class="innerShowcase">
-        Showcase 2
+    <div class="showcaseColumn showcase2">
+        <div class="innerShowcaseBorder">
+            <div class="innerShowcase">
+                <ShowcaseItem class="innerShowcase2" href="#" item={2} backgroundImage='/static/images/showcase3_thumbnail.png' />
+            </div>
+        </div>
     </div>
-    </div></div>
-    <div class="showcaseItem showcase3">
-        <div class="innerShowcaseBorder"><div class="innerShowcase">
-            Showcase 3
-        </div></div>
-        <div class="innerShowcaseBorder"><div class="innerShowcase">
-            Showcase 3
-        </div></div>
+    <div class="showcaseColumn showcase3">
+        <div class="showcase3OuterBox">
+
+            <div class="innerShowcaseBorder">
+                <div class="innerShowcase">
+                    <ShowcaseItem class="innerShowcase3" href="#" item={3} backgroundImage='/static/images/showcase2_thumbnail.png' />
+                </div>
+            </div>
+            <div class="innerShowcaseBorder">
+                <div class="innerShowcase">
+                    <ShowcaseItem class="innerShowcase4" href="#" item={4} />
+                </div>
+            </div>
+
+        </div>
     </div>
 </section>
 
 <style lang="scss">
-    section.showcase {
+    @import '_centerEverything.scss';
+
+    @mixin setBorderBackground($color, $startAngle, $color2) {
+        $frontFalloff: 1deg;
+        $color2Position: 70deg;
+        $startAngle: (($startAngle % (180deg - $frontFalloff)) + (180deg - $frontFalloff));
+        background-image: conic-gradient(
+            transparent 0deg,
+            $color2 $color2Position,
+            transparent ($color2Position + $frontFalloff),
+            transparent calc($startAngle - 50deg), 
+            $color $startAngle, 
+            transparent calc($startAngle + 1deg)
+            
+        );
+    }
+
+    @keyframes borderCycle {
+        100% {
+            transform: rotate(360deg);
+        }
+    }
+
+    $cropAmount: 30px;
+
+    $topLeft: (2 * $cropAmount) 0;
+    $topRight: 100% 0;
+    $bottomRight: calc(100% - (2 * $cropAmount)) 100%;
+    $bottomLeft: 0% 100%;
+
+    section.showcase { // root element
         width: 100%;
-        height: 40vh;
+        height: 60vh;
         position: relative;
         overflow: hidden;
 
         --divisions: 3;
-        $cropAmount: 20px;
+
+        $moreButtonHeight: 20%;
 
         $endcapWidth: calc(100% / var(--divisions));
         $defaultWidth: calc((100% / var(--divisions)) + ($cropAmount * 4));
-        
+
         @mixin calcWidth($endcap) {
-            @if $endcap {
-                width: $endcapWidth;
-            }
-            @else {
-                width: $defaultWidth;
-            }
+            @if $endcap { width: $endcapWidth; }
+            @else { width: $defaultWidth; }
         }
 
         @mixin positionLeft($index) {
@@ -57,22 +98,8 @@
             }
         }
 
-        @mixin centerEverything {
-            background-position: center;
-            background-size: cover;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-        }
-
-        $topLeft: (2 * $cropAmount) 0;
-        $topRight: 100% 0;
-        $bottomRight: calc(100% - (2 * $cropAmount)) 100%;
-        $bottomLeft: 0% 100%;
-
-        // intermediate
-        &>.showcaseItem {
+        // all the columns / direct children of the root element minus overlays
+        &>.showcaseColumn {
             @include calcWidth(false);
             clip-path: polygon($topLeft, $topRight, $bottomRight, $bottomLeft);
             position: absolute;
@@ -81,27 +108,59 @@
             z-index: 3;
             
             @include centerEverything();
+            $margin: 10px; // controls how much space is present between each (actually displayed!) element
+            $border: .5em; // the thickness of the borders
             
-            &>.innerShowcaseBorder {
+            & .innerShowcaseBorder {
                 position: relative;
-                $margin: .5em;
                 width: calc(100% - $margin);
                 height: calc(100% - $margin);
                 clip-path: inherit;
                 background-color: var(--color1);
+                overflow: hidden;
 
-                background-image: conic-gradient(transparent 0deg, transparent 270deg, var(--color5) 280deg);
-                background-color: green;
+                // background-image: conic-gradient(transparent 0deg, transparent 270deg, var(--color5) 280deg);
+                &:before {
+                    content: '';
+                    position: absolute;
+                    $backgroundSize: max(100vw, 100vh);
+                    width: $backgroundSize;
+                    height: $backgroundSize;
+                    clip-path: none;
+                    @include setBorderBackground(var(--color5), 180deg, var(--color2));
+                    
+                    animation: borderCycle 3s ease infinite;
+                }
                 
                 @include centerEverything();
                 
                 &>.innerShowcase {
+                    @include centerEverything();
                     clip-path: inherit;
-                    $border: 1em;
                     width: calc(100% - $border);
                     height: calc(100% - $border);
                     background-color: var(--color1);
                     color: white;
+                    overflow: hidden;
+
+
+                    /* Change all these when the clip path for the corresponding elements change */
+
+                    & :global(.innerShowcase1) {
+                        clip-path: polygon(0 0, $topRight, $bottomRight, 0% 100%);
+                    }
+                    
+                    & :global(.innerShowcase2) {
+                        clip-path: polygon($topLeft, $topRight, $bottomRight, $bottomLeft);
+                    }
+                    
+                    & :global(.innerShowcase3) {
+                        clip-path: polygon($topLeft, $topRight, 100% 100%, calc(($moreButtonHeight / (100% / $cropAmount)) + $border - $margin) 100%);
+                    }
+                    
+                    & :global(.innerShowcase4) {
+                        clip-path: polygon(calc(($moreButtonHeight / (100% / $cropAmount)) + $border - $margin) 0, $topRight, 100% 100%, calc($border - $margin) 100%);
+                    }
                 }
 
             }
@@ -135,28 +194,36 @@
                 }
             }
 
-            $moreButtonHeight: 20%;
+            &>.showcase3OuterBox {
 
-            &:last-child>.innerShowcaseBorder {
+                @include centerEverything();
+                width: calc(100% - $margin);
+                height: calc(100% - $margin);
+                justify-content: space-between;
                 
-                &:first-child {
-                    height: calc(100% - $moreButtonHeight);
+                &>.innerShowcaseBorder:first-child {
+
+                    height: calc(100% - $moreButtonHeight - $margin);
+                    width: calc(100% - $margin); 
+
                     clip-path: polygon(
                         $topLeft,
                         $topRight,
                         100% 100%,
-                        calc(($moreButtonHeight / (100% / $cropAmount)) + .5em) 100%
+                        calc(($moreButtonHeight / (100% / $cropAmount)) + $border - $margin) 100%
                     );
                 }
                 
-                &:last-child {
+                &>.innerShowcaseBorder:last-child {
+                    height: calc($moreButtonHeight - $margin);
+                    width: calc(100% - $margin);
+
                     clip-path: polygon(
-                        calc(($moreButtonHeight / (100% / $cropAmount)) + .5em) 0,
+                        calc(($moreButtonHeight / (100% / $cropAmount)) + $border - $margin) 0,
                         $topRight,
                         100% 100%,
-                        calc(.5em) 100%
+                        calc($border - $margin) 100%
                     );
-                    height: $moreButtonHeight;
                 }
             }
 
@@ -164,18 +231,38 @@
                 @include positionLeft(0);
                 // background-color: #f006;
                 // background-image: url('https://www.travelmanagers.com.au/wp-content/uploads/2012/08/AdobeStock_254529936_Railroad-to-Denali-National-Park-Alaska_750x500.jpg')
+                &>.innerShowcaseBorder:before {
+                    @include setBorderBackground(var(--color5), 80deg, var(--color2));
+                    animation: borderCycle 3s ease infinite .1s;
+                }
+                
             }
-
+            
             &.showcase2 {
                 @include positionLeft(1);
                 // background-color: #0f0;
                 // background-image: url('https://cdn.britannica.com/50/6650-050-E268AC16/coniferous-forest-spruce-trees-forests-boreal-Northern.jpg');
+                &>.innerShowcaseBorder:before {
+                    @include setBorderBackground(var(--color5), 600deg, var(--color2));
+                    animation: borderCycle 3s ease infinite .3s;
+                }
+                
             }
-
+            
             &.showcase3 {
                 @include positionLeft(2);
                 // background-color: #f006;
                 // background-image: url('https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/the-beautiful-cala-goloritze-in-sardinia-royalty-free-image-1149841315-1559070275.jpg');
+                .innerShowcaseBorder:nth-child(1):before {
+                    @include setBorderBackground(var(--color5), 120deg, var(--color2));
+                    animation: borderCycle 3s ease infinite .5s;
+                }
+                
+                .innerShowcaseBorder:nth-child(2):before {
+                    @include setBorderBackground(var(--color3), 180deg, var(--color5));
+                    animation: borderCycle 2s ease infinite .7s;
+                }
+                
             }
 
         }
---
+++ src/lib/routes/index/project-showcase/showcaseItem.svelte
@@ -0,0 +1,74 @@
+<script>
+
+    export let href = "#";
+    export let item;
+    export let backgroundImage = "";
+
+    $: display = (item === 4) ? "Show more" : "Showcase " + item;
+
+</script>
+
+<a class="{$$props.class}" style="{$$props.style}; --bkgImg: url({backgroundImage})" href={href}>
+    <div>
+        <p>
+            <i class="fa-solid fa-arrow-left"></i>
+            {display} 
+            <!-- <FontAwesomeIcon icon="fa-solid fa-arrow-right" /> -->
+        </p>
+    </div>
+</a>
+
+<style lang="scss">
+    @import '_centerEverything.scss';
+
+    a {
+        $margin: 1.5em;
+        width: calc(100% - $margin);
+        height: calc(100% - $margin);
+        position: relative;
+        color: var(--color3);
+        
+        &>div {
+            @include centerEverything();
+
+            width: 100%;
+            height: 100%;
+            background-color: rgba(#15024F, .4);
+            z-index: 2;
+        }
+
+        &::before {
+            content: '';
+            position: absolute;
+            background-image: var(--bkgImg);
+            background-position: center;
+            background-size: 100%;
+            z-index: 1;
+            width: 100%;
+            height: 100%;
+
+            transition: background-size .2s ease-out;
+        }
+
+        &:hover, &:focus {
+            &::before {
+                background-size: 175%;
+            }
+        }
+
+        &:visited {
+            color: var(--color6);
+        }
+
+        @include centerEverything();
+
+        p {
+            font-size: 28px;
+            padding: .4em .7em;
+            // background-color: var(--color4);
+            background-color: rgba(#15024F, .7);
+            border-radius: 8px;
+            color: var(--color3);
+        }
+    }
+</style>
