79374bf1e5c9d62c287f33db07cba1c4232c6e38
enverodie <enverodie@gmail.com>
2022-11-22 03:21:49 -0500

    Made index route distinct from App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,28 +1,5 @@
 <script>
 	import Index from './lib/routes/index/index.svelte';
-	import Header from './lib/routes/index/header/header.svelte';
-	import PageWrapper from './lib/components/page-wrappers/indexPageWrapper.svelte'
-	import Navbar from './lib/components/navbar.svelte';
-	import Showcase from './lib/routes/index/project-showcase/projectShowcase.svelte';
 </script>
 
-<Header />
-<Navbar />
-<PageWrapper>
-
-	<article>
-		<Showcase />
-	</article>
-	
-</PageWrapper>
-
-<style lang="scss">
-
-	article {
-
-		background-color: var(--color1);
-		border: 2px solid hsla(var(--color2H), var(--color2S), var(--color2L), .1); // tied to opacity of square image in _background.scss
-		padding: 1em;
-	}
-
-</style>
+<Index />
--- src/lib/routes/index/index.svelte
+++ src/lib/routes/index/index.svelte
@@ -1,1 +1,27 @@
-<h2>Hello</h2>
+<script>
+    import Header from './lib/routes/index/header/header.svelte';
+	import PageWrapper from './lib/components/page-wrappers/indexPageWrapper.svelte'
+	import Navbar from './lib/components/navbar.svelte';
+	import Showcase from './lib/routes/index/project-showcase/projectShowcase.svelte';
+</script>
+
+<Header />
+<Navbar />
+<PageWrapper>
+
+	<article>
+		<Showcase />
+	</article>
+	
+</PageWrapper>
+
+<style lang="scss">
+
+	article {
+
+		background-color: var(--color1);
+		border: 2px solid hsla(var(--color2H), var(--color2S), var(--color2L), .1); // tied to opacity of square image in _background.scss
+		padding: 1em;
+	}
+
+</style>
