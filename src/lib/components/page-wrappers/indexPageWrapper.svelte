1ac6a2611eb4ee41902a9feecde196c549992a49
enverodie <enverodie@gmail.com>
2022-11-22 23:58:34 -0500

    Initial changes for article alignment Pushing from my main machine

--- src/lib/components/page-wrappers/indexPageWrapper.svelte
+++ src/lib/components/page-wrappers/indexPageWrapper.svelte
@@ -1,9 +1,20 @@
 <script>
 
+    const squareImgSize = 55;
+    const whitespace = 3;
+
+    $: width = document.body.clientWidth;
+    // $: width = window.innerWidth;
+
+    $: leftPaddingPx = ((width) % squareImgSize); // how much further to the right must the padding push the container to align with the next grid box
+    // $: leftPaddingPx = 0;
+    let rightPaddingPx = 0;
 </script>
 
 <main>
-    <slot></slot>
+    <div class="container" style="--leftPadding: {leftPaddingPx}px; --rightPadding: {rightPaddingPx}px;">
+        <slot></slot>
+    </div>
     <aside>
         <div class="stickyItem">
             <p></p>
@@ -16,11 +27,15 @@
     @import '../../routes/background.scss';
 
     main {
-        padding: 2em 3.5em;
+        // padding: 2em 3.5em;
         display: grid;
         grid-template-columns: 7fr 1fr;
         position: relative;
 
+        &>.container {
+            padding-left: calc(var(--boxImgSize) + var(--leftPadding));
+            padding-right: var(--boxImgSize);
+        }
         
         &:before {
             content: '';
--- src/lib/routes/index/index.svelte
+++ src/lib/routes/index/index.svelte
@@ -9,7 +9,7 @@
 <Navbar />
 <PageWrapper>
 
-	<article>
+	<article style="--heightExtension: {0}px; --marginLeftExtension: {0}px; --marginRightExtension: {0}px">
 		<Showcase />
 	</article>
 	
@@ -22,6 +22,11 @@
 		background-color: var(--color1);
 		border: 2px solid hsla(var(--color2H), var(--color2S), var(--color2L), .1); // tied to opacity of square image in _background.scss
 		padding: 1em;
+        margin-top: 58px;
+        margin-left: var(--marginLeftExtension);
+        margin-right: var(--marginRightExtension);
+        margin-bottom: 0;
+        height: auto; // change this later
 	}
 
 </style>
