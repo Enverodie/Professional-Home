eabde6ae654c48b049cfc51a50c08ff93acb0e43
sms10 <sms10@iu.edu>
2022-12-07 04:20:32 -0500

    added city glitch to header

---
+++ src/lib/components/scenesGlitch/cityGlitch.svelte
@@ -0,0 +1,53 @@
+<script>
+    export let width = "auto";
+</script>
+
+<div class="wireframeContainer" style="width: {width}">
+    <img class="layer3" src="/static/svgs/citywireframe/citywireframe layer 3.svg" alt="" aria-hidden />
+    <img class="layer2" src="/static/svgs/citywireframe/citywireframe layer 2.svg" alt="" aria-hidden />
+    <img class="layer1" src="/static/svgs/citywireframe/citywireframe layer 1.svg" alt="" aria-hidden />
+</div>
+
+<style lang="scss">
+
+    @mixin imageDropShadow($color) {
+        filter: 
+            drop-shadow(0 0 4px $color)
+            drop-shadow(0 0 8px $color);
+    }
+
+    .wireframeContainer {
+        position: relative;
+        height: 100%;
+
+        &>*{
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            
+            &:first-child {
+                position: relative;
+            }
+
+            &.layer3 {
+                @include imageDropShadow(#F261FA);
+                z-index: 3;
+            }
+
+            &.layer2 {
+                @include imageDropShadow(#D807E3);
+                z-index: 2;
+            }
+            
+            &.layer1 {
+                @include imageDropShadow(#68036D);
+                z-index: 1;
+            }
+        }
+    }
+
+    img {
+        filter: drop-shadow(0 0 4px var(--color2));
+    }
+</style>
--- src/lib/routes/index/header/header.svelte
+++ src/lib/routes/index/header/header.svelte
@@ -1,8 +1,20 @@
 <script>
-	import GlitchText from '../../../components/enverodieNameGlitch.svelte';
+    import GlitchText from '../../../components/enverodieNameGlitch.svelte';
+	import CityGlitch from './../../../components/scenesGlitch/cityGlitch.svelte';
 
 </script>
 
 <header>
-    <GlitchText fontSize="200px" />
+    <GlitchText fontSize="11.5vw" />
+    <CityGlitch width="44vw" />
 </header>
+
+<style lang="scss">
+    header {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        padding: 4vh 4vw;
+    }
+</style>
