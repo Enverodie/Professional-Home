05dd0938fa61fb1aafb210239223729de2865867
sms10 <sms10@iu.edu>
2022-12-08 02:33:33 -0500

    Enhanced navigation significantly

---
+++ src/lib/components/buttons/humbleAnchor.svelte
@@ -0,0 +1,54 @@
+<script>
+    export let deadLink = false;
+
+</script>
+
+<a 
+    href={$$props.href}
+    class="{deadLink ? "deadLink" : ""} {$$props.class}"
+    >
+    <slot></slot>
+</a>
+
+<style lang="scss">
+a {
+    
+    box-sizing: border-box;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    // this is what I have to do to get text to align vertically. smh.
+
+    --color: var(--color1);
+    background-color: var(--color2);
+    color: var(--color);
+    padding: .5em 2em;
+    margin: 0em .75em;
+    font-weight: 900;
+
+    
+    &:visited {
+        color: var(--color);
+    }
+
+    &:hover, &:focus {
+        /*
+        text-shadow: 
+            1px 1px 1px var(--color7),
+            1px -1px 1px var(--color7),
+            -1px 1px 1px var(--color7),
+            -1px -1px 1px var(--color7);
+        */
+    }
+
+    &.deadLink {
+        background-color: transparent;
+        border: 4px double var(--colorAlert);
+        color: var(--color4);
+
+        &:visited {
+            background-color: var(--colorAlert);
+        }
+    }
+}
+</style>
--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,16 +1,48 @@
 <script>
+	import HumbleAnchor from './buttons/humbleAnchor.svelte';
+
     const NAVIGABLE_ROUTES = ['/', '/Projects'];
+    const INCOMPLETE_ROUTES = ['/Hire', '/Writings', '/CreatedGraphics', '/Blog', '/MinecraftBuilds', '/ContactMe']
     let path = window.location.pathname;
-    
+
+    function getRouteName(route) {
+        return route.slice(1) || "Home";
+    }
+
+    function printClass(index, target, cssClass) {
+        console.log(index, target, cssClass);
+        if (index === target) {
+            return cssClass;
+        }
+        else return "";
+    }
+
+    // Solution provided by Teneff on StackOverflow:
+    // https://stackoverflow.com/a/7888303/15818885
+    function splitPascalCase(string) {
+        return string.split(/(?=[A-Z])/).join(' ');
+    }
 </script>
 
 <nav>
-    <div class="icon">Enverodie</div>
+    <div class="icon">{getRouteName(path)}</div>
     <div class="navElements">
-        {#each NAVIGABLE_ROUTES as routeName}
-            {#if path !== routeName}
-                <a href="{routeName}">{routeName.slice(1) || "Home"}</a>
-            {/if}
+        {#each NAVIGABLE_ROUTES.filter(r => r !== path) as route, index}
+            <HumbleAnchor 
+                href="{route}"
+                class="{printClass(index, 0, "navFirstChild")}"
+                >
+                {splitPascalCase(getRouteName(route))}
+            </HumbleAnchor>
+        {/each}
+        {#each INCOMPLETE_ROUTES as iroute, index}
+            <HumbleAnchor 
+                href="{iroute}"
+                deadLink={true}
+                class="{printClass(index, INCOMPLETE_ROUTES.length-1, "navLastChild")}"
+                >
+                {splitPascalCase(getRouteName(iroute))}
+            </HumbleAnchor>
         {/each}
     </div>
 </nav>
@@ -23,7 +55,7 @@
         z-index: 10;
         margin: 0;
         background-color: var(--color5);
-        padding: 1em;
+        padding: .5em 1em;
         // margin-bottom: 2em;
 
         display: flex;
@@ -31,13 +63,27 @@
         justify-content: space-between;
         align-items: center;
 
-        &>.navElements>a {
-            display: inline-block;
-            background-color: var(--color2);
-            padding: .5em 2em;
-            margin: 0em .75em;
-            font-weight: 700;
+        &>.navElements {
+            display: flex;
+            row-gap: .75em;
+            flex-wrap: nowrap;
+            overflow-x: auto;
+            padding: 0.15em 0.5em;
+
+            &>:global(.navFirstChild) {
+                margin-left: 0;
+            }
+
+            &>:global(.navLastChild) {
+                margin-right: 0;
+            }
         }
+    }
+
+    .icon {
+        font-weight: 700;
+        font-size: 2em;
+        margin-right: 1em;
     }
 
 </style>
