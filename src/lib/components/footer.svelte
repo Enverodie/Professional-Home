594693cfbe5c29eb27402c3e100c5c1817d7df88
sms10 <sms10@iu.edu>
2022-12-08 03:02:06 -0500

    Enhanced footer to include mascot

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -3,13 +3,50 @@
 </script>
 
 <footer>
-Enverodie.com, &copy; 2022 Stephen M. Smith
+    <div class="grid">
+        <div class="column">
+            Enverodie.com, &copy; 2022 Stephen M. Smith
+        </div>
+        <div class="column">
+            <img id="mascotWithSign" src="/static/images/footer.png" alt="Enverodie mascot" />
+        </div>
+    </div>
 </footer>
 
 <style lang="scss">
     footer {
-        padding: 1.2em 0.6em;
         background-color: var(--color5);
         color: var(--color4);
+        position: relative;
+    }
+    
+    .grid {
+        --padding: 1em;
+        box-sizing: border-box;
+        padding: var(--padding) var(--padding) 0;
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        // padding: 1.2em 0.6em;
+        border-collapse: collapse;
+        width: 100%;
+        
+        &>.column {
+            // display: flex;
+
+            &:first-child {
+                padding-bottom: var(--padding);
+                display: flex;
+                align-items: end;
+            }
+    
+            &:last-child {
+    
+            }
+        }    
+    }
+
+    #mascotWithSign {
+        max-height: 20vh;
+        float: right;
     }
 </style>
