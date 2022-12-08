2efcd45b7f82359f1eabc59cf78d3dadcd6c1cb3
sms10 <sms10@iu.edu>
2022-12-08 03:39:41 -0500

    Made about section more readable

--- src/lib/routes/index/about/about.svelte
+++ src/lib/routes/index/about/about.svelte
@@ -14,25 +14,26 @@
 
 <div class="about">
     <div class="grid">
+        <div class="gridItem"></div>
         <div class="gridItem aboutText">
             <AboutMe class="aboutSection">
                 <span>Hi, I'm Stephen M. Smith. <span class="emoji" style="font-size: 1.5em;">😄</span></span><br>
                 <br>
-                I am a {myAge} year old full stack Web Developer based in Bloomington, Indiana. I love experimenting with design, and am particularly fond of working front-end.
-                Javascript is my tool of choice.<br>
-                I also have experience with SQL (MariaDB, PostgreSQL), PHP, Python, Java, and C.<br>
+                I am a {myAge} year old full stack Web Developer, particularly fond of working front-end. I use JavaScript, SQL (MariaDB, PostgreSQL), PHP, Python, Java, and C.<br>
                 <br>
-                I am extremely passionate about coding and enjoy teaching others how to do the same.<br>
+                I am extremely passionate about coding, and enjoy teaching others how to do the same.<br>
                 Long <s>will</s> live the Bogo sort.
             </AboutMe>
             <AboutTheSite class="aboutSection">
-                <span>Enverodie</span> is my personal / professional "username"; a way of personally identifying myself. I thought of the name one day, looked it up on google, found no results, and went with it.<br>
+                <span>Enverodie</span> is my username. I thought of the name one day, looked it up on google, found no results, and went with it.<br>
                 <br>
-                This site is a site showcasing who I am, including all my projects, thoughts, and entertaining things to engage with.<br>
+                This site showcases who I am, including all my projects, thoughts, and entertaining things to engage with.<br>
+                <br>
                 Please, enjoy your stay!
             </AboutTheSite>
             <AboutLinks />
         </div>
+        <div class="gridItem"></div>
         <div class="gridItem">
             <img src="/static/images/about.png" alt="Enverodie mascot waving image" />
         </div>
@@ -48,39 +49,52 @@
         &>* {
             display: none;
 
-            &:first-child {
+            &.aboutText {
                 display: block;
             }
         }
 
     }
 
-    .aboutText :global(.aboutSection) {
-        text-align: justify;
-        text-align-last: center;
-        vertical-align: text-top;
-    }
-    .aboutText :global(.aboutSection h2) {
-        // text-align: center;
-    }
-    .aboutText :global(.aboutSection hr) {        
-        height: 2px;
-        background-color: var(--color7);
-        border-radius: 10px;
-        text-decoration: none;
-        border-color: transparent;
-        box-shadow: var(--hoverShadow);
+    .aboutText {
+        
+        :global(.aboutSection) {
+            text-align: justify;
+            text-align-last: center;
+            vertical-align: text-top;
+        }
+
+        :global(.aboutSection h2) {
+            // text-align: center;
+        }
+
+        :global(.aboutSection hr) {        
+            height: 2px;
+            background-color: var(--color7);
+            border-radius: 10px;
+            text-decoration: none;
+            border-color: transparent;
+            box-shadow: var(--hoverShadow);
+        }
+
+        :global(.aboutSection p) {        
+            padding: 0 2em;
+            margin-bottom: 2em;
+        }
     }
 
 @media only screen and (min-width: 1000px) {
     .grid {
-        grid-template-columns: 4fr 3fr;
+        grid-template-columns: .25fr 3fr .5fr 3fr;
 
         &>* {
             display: flex;
             justify-content: center;
             align-items: center;
-            padding: 1em;
+
+            &:last-child {
+                padding: 1em;
+            }
 
             img {
                 max-width: 100%;
