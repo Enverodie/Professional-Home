84f161d6ef0cc3addf3af1c336904b31d6619dbd
sms10 <sms10@iu.edu>
2023-01-25 22:15:20 -0500

    Grid page wrappers can now can pass down padding

--- src/lib/components/page-wrappers/grid.svelte
+++ src/lib/components/page-wrappers/grid.svelte
@@ -2,18 +2,21 @@
 
     /* The Grid component establishes a full-page main section with horizontal spacing aligning with a background grid image. */
 
+    import { onMount, setContext } from 'svelte';
 	import Footer from './../footer.svelte';
 	import { SQUARE_IMG_SIZE, SQUARE_IMG_WHITESPACE } from './../../constants/grid.js';
     import Navigation from '../navigation/navigation.svelte';
     export let navigation = true;
     export let footer = true;
+    export let backgroundPosition = "absolute";
 
     const boxesFromEdges = 2;
     let divSection; // set the style on this element directly
     let width;
     let divWidth;
-    
-    $: if (divSection !== undefined && width !== undefined) {
+
+    function getPadding() {
+        if (!width) return 0;
         let padding = (
             (
                 (
@@ -22,17 +25,35 @@
                 ) % SQUARE_IMG_SIZE
             ) + SQUARE_IMG_WHITESPACE
         );
-
-        divSection.style.paddingLeft = `${padding}px`;
-        divSection.style.paddingRight = `${padding}px`;
+        return padding;
     }
 
+    function updatePadding(outputElement = false) {
+        let padding = getPadding();
+
+        // THIS IS WHERE THE PADDING IS PUT INTO THE STYLE
+        function setPadding(HTMLElement) {
+            HTMLElement.style.paddingLeft = `${padding}px`;
+            HTMLElement.style.paddingRight = `${padding}px`;
+        }
+        if (outputElement) setPadding(outputElement);
+        else setContext("setPadding", setPadding);
+    }
+
+    onMount(() => {
+        updatePadding(divSection);
+    });
+  
+
 </script>
+
+<svelte:window on:resize={() => {updatePadding(divSection)}} />
 
 {#if (navigation)}
     <Navigation />
 {/if}
-<main bind:clientWidth={width} >
+<main style="--bPosition: {backgroundPosition}" bind:clientWidth={width} >
+    <slot name="nonPadded"></slot>
     <div class="staticPadding">
         <div 
             bind:this={divSection} 
@@ -59,7 +80,7 @@
             content: '';
             width: 100%;
             height: 100%;
-            position: absolute;
+            position: var(--bPosition);
             @include gridBackgroundImageNoFilter();
             z-index: -1;
         }
--- src/lib/components/page-wrappers/gridWSideNav.svelte
+++ src/lib/components/page-wrappers/gridWSideNav.svelte
@@ -4,7 +4,7 @@
      * allowing for a side navigation component, aligning with a background grid image. 
      */
     
-    import { onMount } from 'svelte';
+    import { onMount, setContext } from 'svelte';
     import Navigation from '../navigation/navigation.svelte';
 	import Footer from './../footer.svelte';
 	import { SQUARE_IMG_SIZE, SQUARE_IMG_WHITESPACE } from './../../constants/grid.js';
@@ -19,66 +19,94 @@
     let mainElement, navContainerElement;
 
     let windowHeight;
-    let mainH, mainClientY;
+    let mainHeight, mainClientY;
     let navboxH = 0, navboxClientY;
 
     let divSection; // set the style on this element directly
     let width;
     let divWidth;
 
+    function getPaddingLeft() {
+        if (!width) return 0;
+        let padding = (
+            (
+                (
+                    (width/2.0) - 
+                    (SQUARE_IMG_SIZE/2.0)
+                ) % SQUARE_IMG_SIZE
+            ) + SQUARE_IMG_WHITESPACE
+        );
+        return padding;
+    }
+
+    function getPaddingRight() {
+        if (!width) return 0;
+        let padding = (
+            (
+                (divWidth/2.0) - 
+                ((width - divWidth)/2) -
+                (SQUARE_IMG_SIZE/2.0)
+            ) % SQUARE_IMG_SIZE
+        ) + SQUARE_IMG_WHITESPACE;
+        return padding;
+    }
+
+    function updatePadding(outputElement = false) {
+        let paddingL = getPaddingLeft();
+        let paddingR = getPaddingRight();
+
+        function setPadding(HTMLElement) {
+            HTMLElement.style.paddingLeft = `${paddingL}px`;
+            HTMLElement.style.paddingRight = `${paddingR}px`;
+        }
+
+        if (outputElement) setPadding(outputElement);
+        else setContext("setPadding", setPadding);
+    }
+
     function updateGeometryData() {
         if (mainElement) {
             let mE = mainElement.getBoundingClientRect();
-            mainH = mE.height;
+            mainHeight = mE.height;
             mainClientY = mE.y;
         } 
         if (navContainerElement) {
             let nCE = navContainerElement.getBoundingClientRect();
             navboxClientY = nCE.y;
-            navboxH = windowHeight - navboxClientY - Math.max((-mainH - mainClientY + windowHeight), 0);
+            navboxH = windowHeight - navboxClientY - Math.max((-mainHeight - mainClientY + windowHeight), 0);
         }
     }
 
     onMount(() => {
+        updatePadding(divSection);
         updateGeometryData();
     })
 
-    $: if (divSection !== undefined && width !== undefined) {
-        divSection.style.paddingLeft = `${
-            (
-                (
-                    (
-                        (width/2.0) - 
-                        (SQUARE_IMG_SIZE/2.0)
-                    ) % SQUARE_IMG_SIZE
-                ) + SQUARE_IMG_WHITESPACE
-            )
-        }px`;
-        divSection.style.paddingRight = `${
-            (
-                
-                (
-                    (divWidth/2.0) - 
-                    ((width - divWidth)/2) -
-                    (SQUARE_IMG_SIZE/2.0)
-                ) % SQUARE_IMG_SIZE
-            ) + SQUARE_IMG_WHITESPACE
-        }px`;
-    }
-
 </script>
 
+<!-- Performs logic that updates integer value of element being viewed -->
 <IntersectionHandler bind:trackedIDs bind:position />
-<svelte:window bind:innerHeight={windowHeight} on:resize={updateGeometryData} on:scroll={updateGeometryData} />
+
+<!--  -->
+<svelte:window 
+    bind:innerHeight={windowHeight} 
+    on:resize={() => {updatePadding(divSection); updateGeometryData()}} 
+    on:scroll={updateGeometryData} />
 
 {#if (navigation)}
     <Navigation>
+        <!-- The diamonds + bounding box for diamonds on the side of the screen -->
         <div slot="inPageNav" class="inPageNav" style="--height: {navboxH}px" bind:this={navContainerElement}>
+
+            <!-- The diamonds component -->
             <SidebarNav bind:scrollToSections={trackedIDs} bind:position />
         </div>
     </Navigation>
 {/if}
+
 <main class="mainGrid" bind:this={mainElement} bind:clientWidth={width} >
+    
+    <!-- This element can get padding based on the background grid -->
     <div 
         bind:this={divSection} 
         bind:clientWidth={divWidth} 
@@ -88,10 +116,12 @@
     </div>
     <aside class="stickyItem"></aside> <!-- May be useful later; otherwise used as whitespace for the sidebar nav -->
 </main>
+
 {#if (footer)}
     <Footer />
 {/if}
     
+
 <style lang="scss">
 
     @import '../route-specific/background.scss';
