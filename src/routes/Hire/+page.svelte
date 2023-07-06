62de0e34bf23335ea1819eb340c7987313b4a983
enverodie <enverodie@gmail.com>
2023-07-06 16:40:04 -0400

    Removed placeholder content

--- src/routes/Hire/+page.svelte
+++ src/routes/Hire/+page.svelte
@@ -7,32 +7,5 @@
 </script>
 
 <PageWrapper backgroundPosition="fixed">
-    <PositionInWrapper>
-        <ArticleBox>Test</ArticleBox>
-    </PositionInWrapper>
-    <PositionInWrapper>
-        <SectionBox>Test</SectionBox>
-    </PositionInWrapper>
-    <div>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac orci phasellus egestas tellus. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. Tellus integer feugiat scelerisque varius. Urna nunc id cursus metus aliquam. Et magnis dis parturient montes nascetur ridiculus. Luctus accumsan tortor posuere ac ut consequat semper viverra nam. Egestas egestas fringilla phasellus faucibus scelerisque. Arcu cursus vitae congue mauris rhoncus. Aliquam etiam erat velit scelerisque in dictum non consectetur.
 
-Scelerisque purus semper eget duis at. Quam lacus suspendisse faucibus interdum posuere lorem ipsum. Posuere morbi leo urna molestie at. Platea dictumst vestibulum rhoncus est pellentesque elit. Gravida neque convallis a cras semper auctor. Massa id neque aliquam vestibulum morbi blandit. At urna condimentum mattis pellentesque id nibh tortor id. Eu consequat ac felis donec. Adipiscing elit pellentesque habitant morbi tristique senectus et netus. Dolor purus non enim praesent elementum facilisis. Enim facilisis gravida neque convallis a cras semper. Ullamcorper eget nulla facilisi etiam dignissim diam quis enim. Sagittis nisl rhoncus mattis rhoncus urna neque viverra justo. Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan. Vulputate ut pharetra sit amet aliquam id diam maecenas ultricies. Donec ac odio tempor orci dapibus ultrices in iaculis nunc. Purus faucibus ornare suspendisse sed nisi lacus sed. Nisl condimentum id venenatis a condimentum vitae sapien pellentesque. Molestie ac feugiat sed lectus vestibulum mattis ullamcorper. Fusce id velit ut tortor pretium.
-
-Aliquet sagittis id consectetur purus ut faucibus pulvinar. Auctor elit sed vulputate mi. In nibh mauris cursus mattis molestie a iaculis at erat. Leo urna molestie at elementum. Amet justo donec enim diam vulputate ut pharetra. Lectus magna fringilla urna porttitor rhoncus dolor purus. Diam vel quam elementum pulvinar etiam non quam. Nunc sed id semper risus in hendrerit. Nisl tincidunt eget nullam non nisi est. Convallis posuere morbi leo urna molestie at elementum eu. Interdum varius sit amet mattis vulputate. Quis imperdiet massa tincidunt nunc pulvinar sapien et ligula. Elit duis tristique sollicitudin nibh sit amet commodo nulla facilisi. Ac odio tempor orci dapibus ultrices in iaculis. Pharetra diam sit amet nisl. Condimentum mattis pellentesque id nibh tortor id aliquet lectus.
-
-Mollis aliquam ut porttitor leo a diam sollicitudin tempor. Egestas erat imperdiet sed euismod nisi porta lorem mollis. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Metus dictum at tempor commodo ullamcorper. Ut etiam sit amet nisl purus in mollis nunc. Curabitur vitae nunc sed velit dignissim. Ullamcorper a lacus vestibulum sed arcu. Enim ut sem viverra aliquet eget. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Imperdiet sed euismod nisi porta lorem mollis aliquam. Semper eget duis at tellus at. Odio tempor orci dapibus ultrices in iaculis. Ullamcorper malesuada proin libero nunc consequat. Quam id leo in vitae. Quam adipiscing vitae proin sagittis nisl rhoncus. Ultricies mi eget mauris pharetra et. Vulputate odio ut enim blandit volutpat.
-
-Facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui. Tristique et egestas quis ipsum suspendisse ultrices gravida. Ut morbi tincidunt augue interdum velit. Varius morbi enim nunc faucibus a pellentesque sit amet. Aenean et tortor at risus viverra adipiscing at. Est lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque. Magna fringilla urna porttitor rhoncus dolor purus. Lacus suspendisse faucibus interdum posuere lorem ipsum. Tempus imperdiet nulla malesuada pellentesque elit. Amet cursus sit amet dictum sit. At lectus urna duis convallis convallis tellus id. Feugiat pretium nibh ipsum consequat. Iaculis at erat pellentesque adipiscing commodo elit at. Tempus egestas sed sed risus pretium quam. Fusce id velit ut tortor pretium viverra suspendisse.
-    </div>
-    <div>
-        Hey
-    </div>
 </PageWrapper>
-
-<style lang="scss">
-    
-    header {
-        // position: absolute;
-    }
-
-</style>
