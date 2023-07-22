865ad4947261c883577c30c348069d5aa79640d1
sms10 <sms10@iu.edu>
2023-07-22 01:51:53 -0400

    Added test route to test component

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import PageWrapper from '$lib/components/layout/grid.svelte';
+	import ArticleBox from './../../lib/components/contentboxes/articleBox.svelte';
+	import Showcase from './../../lib/components/specialFX/showcase/showcase.svelte';
+
+</script>
+<PageWrapper>
+    <ArticleBox>
+        <Showcase style="height: 40vh" images={[
+            {src: '/creativePosts/Planet Project.png', alt: 'planet', description: 'I made a planet'},
+            {src: '/creativePosts/Planet Project.png', alt: 'planet', description: 'I made a planet'},
+            {src: '/creativePosts/Planet Project.png', alt: 'planet', description: 'I made a planet'},
+            {src: '/creativePosts/Planet Project.png', alt: 'planet', description: 'I made a planet'},
+            {src: '/creativePosts/Planet Project.png', alt: 'planet', description: 'I made a planet'},
+            {src: '/creativePosts/Planet Project.png', alt: 'planet', description: 'I made a planet'},
+            {src: '/creativePosts/Planet Project.png', alt: 'planet', description: 'I made a planet'},
+            {src: '/creativePosts/Planet Project.png', alt: 'planet', description: 'I made a planet'},
+        ]} />
+    </ArticleBox>
+</PageWrapper>
