2dda1cdae6e32a1471f16758bcbde36d01c14357
enverodie <enverodie@gmail.com>
2023-08-03 23:48:36 -0400

    Removed node-canvas dependency

--- src/lib/constants/grid.js
+++ src/lib/constants/grid.js
@@ -1,17 +1,6 @@
-import { createCanvas } from "canvas";
-
 export const SQUARE_IMG_SIZE = 55;
 export const SQUARE_IMG_WHITESPACE = 3;
 
 export const createBackgroundGridImage = (hexColor) => {
-    let image = createCanvas(SQUARE_IMG_SIZE, SQUARE_IMG_SIZE);
-    let ctx = image.getContext('2d');
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = hexColor;
-    ctx.strokeRect(
-        SQUARE_IMG_WHITESPACE+1, 
-        SQUARE_IMG_WHITESPACE+1, 
-        (SQUARE_IMG_SIZE - (2*SQUARE_IMG_WHITESPACE) - 2), 
-        (SQUARE_IMG_SIZE - (2*SQUARE_IMG_WHITESPACE) - 2));
-    return image.toDataURL();
+    return `data:image/svg+xml;utf8,<svg width="55" height="56" viewBox="0 0 55 56" fill="none" xmlns="http://www.w3.org/2000/svg"><rect x="4" y="4.30762" width="47" height="47" stroke="${hexColor}" stroke-width="2"/></svg>`;
 }
