a90eb0860ed81d683752a58426564e114e476bd5
enverodie <enverodie@gmail.com>
2023-08-10 11:36:46 -0400

    Implemented experience cycling

--- src/routes/contact/experience.svelte
+++ src/routes/contact/experience.svelte
@@ -7,43 +7,75 @@
 
     let categories = [
         { name: 'JS libraries', examples: [
-            { name: 'React', icon: '', description: '' },
-            { name: 'Svelte', icon: '', description: '' }
+            { id: 0, name: 'React', icon: '/svgs/technologies-icons/React Logo.svg', description: '' },
+            { id: 1, name: 'Svelte', icon: '/svgs/technologies-icons/Svelte_Logo.svg', description: '' }
         ] },
         { name: 'Vanilla technologies', examples: [
-            { name: 'HTML', icon: '', description: '' },
-            { name: 'CSS', icon: '', description: '' },
-            { name: 'JavaScript', icon: '', description: '' }
+            { id: 2, name: 'HTML', icon: '/svgs/technologies-icons/HTML5 Logo.svg', description: '' },
+            { id: 3, name: 'CSS', icon: '/svgs/technologies-icons/CSS3_logo_and_wordmark.svg', description: '' },
+            { id: 4, name: 'JavaScript', icon: '/svgs/technologies-icons/JavaScript-logo.png', description: '' }
         ] },
         { name: 'CSS extensions', examples: [
-            { name: 'SASS/SCSS', icon: '', description: '' },
-            {}
+            { id: 5, name: 'SASS/SCSS', icon: '/svgs/technologies-icons/sass-seal-color-reversed.png', description: '' },
         ] },
         { name: 'Programming languages', examples: [
-            { name: 'Java', icon: '', description: '' },
-            { name: 'C', icon: '', description: '' },
-            { name: 'Python', icon: '', description: '' },
-            { name: 'PHP', icon: '', description: '' }
+            { id: 6, name: 'Java', icon: '', description: '' },
+            { id: 7, name: 'C', icon: '', description: '' },
+            { id: 8, name: 'Python', icon: '', description: '' },
+            { id: 9, name: 'PHP', icon: '', description: '' }
         ] },
         { name: 'Databases', examples: [
-            { name: 'MySQL', icon: '', description: '' },
-            { name: 'MariaDB', icon: '', description: '' },
-            { name: 'PostgreSQL', icon: '', description: '' },
-            { name: 'MongoDB', icon: '', description: '' }
+            { id: 10, name: 'MySQL', icon: '', description: '' },
+            { id: 11, name: 'MariaDB', icon: '', description: '' },
+            { id: 12, name: 'PostgreSQL', icon: '', description: '' },
+            { id: 13, name: 'MongoDB', icon: '', description: '' }
         ] },
     ];
-    let activeCategory = 0;
+    let activeCategory = 1;
+    let firstItemIndex = 0;
+
+    let cycle = true;
+    function generateCycleableArray(firstItemIndex, activeCategory) {
+        let startArrayHalf = categories[activeCategory].examples.slice(firstItemIndex);
+        let lastArrayHalf = categories[activeCategory].examples.slice(0, firstItemIndex);
+        let uniqueArray = [...startArrayHalf, ...lastArrayHalf];
+        let hideawayElement = {...uniqueArray[uniqueArray.length - 1], id: uniqueArray[uniqueArray.length-1].id + uniqueArray.length};
+        return [
+            hideawayElement,
+            ...uniqueArray
+        ]
+    }
+    $: cycleableArray = generateCycleableArray(firstItemIndex, activeCategory);
+
+    const animationTimeMS = 1000;
+    
+    $: animateForward = false;
+    $: animateBackward = false;
+    function updatePosition(moveBackwards) {
+        if (moveBackwards) {
+            setTimeout(() => { animateBackward = false }, animationTimeMS);
+            firstItemIndex = (firstItemIndex-1 < 0) ? categories[activeCategory].examples.length - 1 : firstItemIndex-1;
+            animateBackward = true;
+        }
+        else {
+            setTimeout(() => { animateForward = false }, animationTimeMS);
+            firstItemIndex = (firstItemIndex+1) % categories[activeCategory].examples.length
+            animateForward = true;
+        }
+    }
 
     let cycleInterval;
     onMount(() => {
         cycleInterval = setInterval(() => {
-            activeCategory = (activeCategory+1) % categories.length
+            if (cycle) {
+                updatePosition();
+            } 
         }, timeToCycle)
     });
 
     onDestroy(() => {
         clearInterval(cycleInterval);
-    })
+    });
 
 </script>
 
@@ -51,17 +83,68 @@
     <div class="activeCategory">
         Experienced in
         <span class="active">{categories[activeCategory].name}</span>
-        <div class="cards">
-            {#each categories[activeCategory].examples as item}
-                <ExperienceCard item={item} />
-            {/each}
+        <div 
+            class="cardsContainer" 
+            on:mouseenter={() => {cycle = false}} 
+            on:mouseleave={() => {cycle = true}} >
+            <button class="button1" on:click={() => {updatePosition(true)}}>&lsaquo;</button>
+            <div style="overflow: hidden; height: 100%; flex: 1; margin: 0 2%;">
+                <div class="cards" class:animateForward={animateForward} class:animateBackward={animateBackward} style="--animationTimeMS: {animationTimeMS}ms;">
+                    {#each cycleableArray as item, index (item.id)}
+                        <ExperienceCard item={item} removeTabbable={animateForward || animateBackward || (index < 1 || index > 2)} />
+                    {/each}
+                </div>
+            </div>
+            <button class="button1" on:click={() => {updatePosition()}}>&rsaquo;</button>
         </div>
     </div>
 </div>
 
 <style lang="scss">
 
+    .button1 {
+        font-size: var(--titleLarge);
+        margin-top: 0;
+        margin-bottom: 0;
+    }
+
     .active {
         display: inline-block;
+    }
+
+    .cardsContainer {
+        height: 300px;
+        display: flex;
+    }
+
+    .cards {
+        white-space: nowrap;
+        position: relative;
+        height: 100%;
+        left: -51%;
+        &.animateForward {
+            animation: scroll var(--animationTimeMS) ease-in-out;
+        }
+        &.animateBackward {
+            animation: scrollBackward var(--animationTimeMS) ease-in-out;
+        }
+    }
+
+    @keyframes scroll {
+        from {
+            left: 0%;
+        }
+        to {
+            left: -51%;
+        }
+    }
+
+    @keyframes scrollBackward {
+        from {
+            left: -102%;
+        }
+        to {
+            left: -51%;
+        }
     }
 </style>
--- src/routes/contact/experienceCard.svelte
+++ src/routes/contact/experienceCard.svelte
@@ -1,43 +1,87 @@
 <script>
     
     export let item;
+    export let removeTabbable;
     let { name, icon, description } = item;
     let showDescription = false;
 
 </script>
 
 <div class="card" class:showDescription>
-    <div>{name}</div>
-    <img src={icon} alt={name + " icon"} />
-    <button on:click={() => {showDescription = !showDescription}}>
-        ?
-    </button>
+    <div class="textIcon">
+        <div class="name">{name}</div>
+        <img src={icon} alt={name + " icon"} />
+        <button class="button1 descriptionButton" on:click={() => {showDescription = !showDescription}} tabindex={removeTabbable? -1 : 0}>
+            ?
+        </button>
+    </div>
     <div class="description">{description}</div>
 </div>
 
 <style lang="scss">
     
     .card {
-        width: 50%;
+        &:first-child { margin-left: 0; }
+        &:last-child { margin-right: 0; }
+        box-sizing: border-box;
+        padding: var(--defaultPadding);
+        margin: 0 1%;
+        width: 49%;
         height: 100%;
         text-align: center;
         display: inline-block;
         flex-direction: column;
         justify-content: center;
-        overflow: auto;
+        overflow: hidden;
+        background-color: hsla(var(--color2H), var(--color2S), var(--color2L), var(--boxOpacity));
+        position: relative;
 
-        .description { display: none; }
+        .description { display: none; top: 100%; }
+        .descriptionButton {
+            z-index: 4;
+            align-self: flex-end;
+            width: 40px;
+            height: 40px;
+            padding: 1em; // idk why but this fixes the width and height
+        }
 
         &.showDescription {
 
             .description {
+                background-color: hsla(var(--color1H), var(--color1S), var(--color1L), .8);
+                white-space: normal;
+                box-sizing: border-box;
+                overflow: auto;
+                padding: var(--defaultPadding);
+                z-index: 3;
                 display: block;
+                position: absolute;
                 top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
                 transform: top .5s ease-in-out;
             }
         }
 
-        &>img { flex: 1; }
+        .textIcon {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            height: 100%;
+            gap: var(--defaultPadding);
+
+            &>.name {
+                font-size: var(--textLarge);
+            }
+            
+            &>img {
+                width: 100%; 
+                height: 100%; 
+                object-fit: contain;
+            }
+        }
+
     }
 
 </style>
