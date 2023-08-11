7707d17ffbf3f27a81810ab93143b613796cdf11
enverodie <enverodie@gmail.com>
2023-08-11 15:49:21 -0400

    Allowed changing category

--- src/routes/contact/experience.svelte
+++ src/routes/contact/experience.svelte
@@ -7,34 +7,35 @@
 
     let categories = [
         { name: 'JS libraries', examples: [
-            { id: 0, name: 'React', icon: '/svgs/technologies-icons/React Logo.svg', description: '' },
-            { id: 1, name: 'Svelte', icon: '/svgs/technologies-icons/Svelte_Logo.svg', description: '' }
+            { id: 10, name: 'React', icon: '/svgs/technologies-icons/React Logo.svg', description: '' },
+            { id: 30, name: 'Svelte', icon: '/svgs/technologies-icons/Svelte_Logo.svg', description: '' }
         ] },
         { name: 'Vanilla technologies', examples: [
-            { id: 2, name: 'HTML', icon: '/svgs/technologies-icons/HTML5 Logo.svg', description: '' },
-            { id: 3, name: 'CSS', icon: '/svgs/technologies-icons/CSS3_logo_and_wordmark.svg', description: '' },
-            { id: 4, name: 'JavaScript', icon: '/svgs/technologies-icons/JavaScript-logo.png', description: '' }
+            { id: 50, name: 'HTML', icon: '/svgs/technologies-icons/HTML5 Logo.svg', description: '' },
+            { id: 70, name: 'CSS', icon: '/svgs/technologies-icons/CSS3_logo_and_wordmark.svg', description: '' },
+            { id: 90, name: 'JavaScript', icon: '/svgs/technologies-icons/JavaScript-logo.png', description: '' }
         ] },
         { name: 'CSS extensions', examples: [
-            { id: 5, name: 'SASS/SCSS', icon: '/svgs/technologies-icons/sass-seal-color-reversed.png', description: '' },
+            { id: 110, name: 'SASS/SCSS', icon: '/svgs/technologies-icons/sass-seal-color-reversed.png', description: '' },
         ] },
         { name: 'Programming languages', examples: [
-            { id: 6, name: 'Java', icon: '', description: '' },
-            { id: 7, name: 'C', icon: '', description: '' },
-            { id: 8, name: 'Python', icon: '', description: '' },
-            { id: 9, name: 'PHP', icon: '', description: '' }
+            { id: 130, name: 'Java', icon: '', description: '' },
+            { id: 150, name: 'C', icon: '', description: '' },
+            { id: 170, name: 'Python', icon: '', description: '' },
+            { id: 190, name: 'PHP', icon: '', description: '' }
         ] },
         { name: 'Databases', examples: [
-            { id: 10, name: 'MySQL', icon: '', description: '' },
-            { id: 11, name: 'MariaDB', icon: '', description: '' },
-            { id: 12, name: 'PostgreSQL', icon: '', description: '' },
-            { id: 13, name: 'MongoDB', icon: '', description: '' }
+            { id: 210, name: 'MySQL', icon: '', description: '' },
+            { id: 230, name: 'MariaDB', icon: '', description: '' },
+            { id: 250, name: 'PostgreSQL', icon: '', description: '' },
+            { id: 270, name: 'MongoDB', icon: '', description: '' }
         ] },
     ];
-    let activeCategory = 1;
+    let activeCategory = 0;
     let firstItemIndex = 0;
 
-    let cycle = true;
+    let cardsHovered = false;
+    $: cycle = (!cardsHovered) && categories[activeCategory].examples.length >= 3;
     function generateCycleableArray(firstItemIndex, activeCategory) {
         let startArrayHalf = categories[activeCategory].examples.slice(firstItemIndex);
         let lastArrayHalf = categories[activeCategory].examples.slice(0, firstItemIndex);
@@ -64,6 +65,16 @@
         }
     }
 
+    function updateActiveCategory(moveBackwards) {
+        if (moveBackwards) {
+            activeCategory = (activeCategory-1 < 0) ? categories.length - 1 : activeCategory-1;
+        }
+        else {
+            activeCategory = (activeCategory+1) % categories.length;
+        }
+        firstItemIndex = 0;
+    }
+
     let cycleInterval;
     onMount(() => {
         cycleInterval = setInterval(() => {
@@ -81,13 +92,19 @@
 
 <div class='container'>
     <div class="activeCategory">
-        Experienced in
-        <span class="active">{categories[activeCategory].name}</span>
+        <div class="activeLabel">
+            Experienced in
+            <div class="active">
+                <button class="button1" on:click={() => {updateActiveCategory(true)}}>&lsaquo;</button>
+                {categories[activeCategory].name}
+                <button class="button1" on:click={() => {updateActiveCategory()}}>&rsaquo;</button>
+            </div>
+        </div>
         <div 
             class="cardsContainer" 
-            on:mouseenter={() => {cycle = false}} 
-            on:mouseleave={() => {cycle = true}} >
-            <button class="button1" on:click={() => {updatePosition(true)}}>&lsaquo;</button>
+            on:mouseenter={() => {cardsHovered = true}} 
+            on:mouseleave={() => {cardsHovered = false}} >
+            <button class="button1" class:invisible={categories[activeCategory].examples.length < 3} on:click={() => {updatePosition(true)}}>&lsaquo;</button>
             <div style="overflow: hidden; height: 100%; flex: 1; margin: 0 2%;">
                 <div class="cards" class:animateForward={animateForward} class:animateBackward={animateBackward} style="--animationTimeMS: {animationTimeMS}ms;">
                     {#each cycleableArray as item, index (item.id)}
@@ -95,12 +112,16 @@
                     {/each}
                 </div>
             </div>
-            <button class="button1" on:click={() => {updatePosition()}}>&rsaquo;</button>
+            <button class="button1" class:invisible={categories[activeCategory].examples.length < 3} on:click={() => {updatePosition()}}>&rsaquo;</button>
         </div>
     </div>
 </div>
 
 <style lang="scss">
+
+    .invisible {
+        display: none;
+    }
 
     .button1 {
         font-size: var(--titleLarge);
@@ -108,9 +129,28 @@
         margin-bottom: 0;
     }
 
-    .active {
-        display: inline-block;
+    .activeLabel {
+        font-size: var(--titleDefault);
+        margin: var(--defaultPadding) 0;
+        display: flex;
+        flex-wrap: wrap;
+        width: 100%;
+        box-sizing: border-box;
+        gap: 1ch;
+        align-items: center;
+        text-align: center;
+
+        .active {
+            background-color: var(--color5);
+            border-radius: var(--defaultBorderRadius);
+            flex: 1;
+            padding: .5em;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
     }
+
 
     .cardsContainer {
         height: 300px;
